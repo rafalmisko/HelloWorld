cc37ab8f6f68950608f52fba353f4f4d41cffd4e
Rafał Miśkowiec <sbgrafo@gmail.com>
2024-04-21 20:29:58 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        {
+            //camelCase
+            string firstAndLastName = "Rafał";
+
+            //Upper_Case
+            const double PI_NUMBER = 3.14; 
+            Console.WriteLine(firstAndLastName);
+            Console.WriteLine("Nazywam się Rafał");
+            if (PI_NUMBER > 2)
+            {
+
+            }
+        }
+
+        private string WritePINumber(double pi)
+        {
+            return ("3.14"); 
+        }
+        }
+      
+    }
+ 
+
+ 
